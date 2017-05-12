5997636e6891a9f725fbb85ca182bf10d8ea5ca2
Trevor Burke <trevorpburke@gmail.com>
2017-05-12 13:13:04 -0700

    renamed extract_tools.py to sql.py, modified setup.py, and modified __init__.py

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
-from disutils.core import setup
-setup(
-    name = 'quickextract',
-    packages = ['quickextract'],
-    version = '0.1',
-    description = 'A quick tool to generate basic CSV or Excel files from SQL
-    queries',
-    author = 'Trevor Burke',
-    author_email = 'trevorpburke@gmail.com'
-    url = ''
-    download_url = ''
-    keywords = ['sql', 'extract', 'pandas', 'csv']
-    classfiers = [],
-)
+from setuptools import setup
 
-
+setup(name='quickextract',
+      version='0.1',
+      description='A quick tool to generate basic CSV or Excel files from SQL\
+      queries',
+      author='Trevor Burke',
+      author_email='trevorpburke@gmail.com',
+      license='MIT',
+      packages=['quickextract'],
+      zip_safe=False)
